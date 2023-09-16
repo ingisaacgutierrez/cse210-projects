d40c8b18f33c0e542baaa65c745c32f8e8ebd198
ingisaacgutierrez <igutierrezsaltos@gmail.com>
2023-09-16 02:27:40 -0500

    Practicing if statement assigment

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,9 +1,40 @@
 using System;
+using System.Security.Cryptography;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
-    }
+        Console.Write("What is your grade percentage? ");
+        string grade = Console.ReadLine();
+        if (int.Parse(grade) >= 90)
+        {
+            Console.WriteLine($"Your grade is A");
+        }
+        else if (int.Parse(grade) >= 80)
+        {
+            Console.WriteLine($"Your grade is B");
+        }
+        else if (int.Parse(grade) >= 70)
+        {
+            Console.WriteLine($"Your grade is C");
+        } 
+        else if (int.Parse(grade) >= 60)
+        {
+            Console.WriteLine($"Your grade is D");
+        } 
+        else if (int.Parse(grade) < 600)
+        {
+            Console.WriteLine($"Your grade is F");
+        }
+
+        if ( int.Parse(grade) >= 70)
+        {
+            Console.WriteLine("You passed!");
+        }
+        else
+        {
+            Console.WriteLine("Better luck next time!");
+        }
+    }  
 }
