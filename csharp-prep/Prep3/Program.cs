f52ffbabbfc025b10ecb64544cc7519cdea5369d
ingisaacgutierrez <igutierrezsaltos@gmail.com>
2023-09-19 19:09:32 -0500

    pre 3 assigment

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int magicnum = randomGenerator.Next(1, 101);
+        
+        int gms = -1;
+        while (gms != magicnum){
+            Console.Write("What is your guess? ");
+            gms = int.Parse(Console.ReadLine());
+
+            if (gms > magicnum){
+                Console.WriteLine("Lower");
+            }
+            else if (gms < magicnum){
+                Console.WriteLine("Higher");
+            }
+            else{
+                 Console.WriteLine("You guessed it!");
+            }
+        
+        } 
     }
 }
