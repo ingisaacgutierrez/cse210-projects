c7344d5a431d972fa0300e2282d839c0b2fda5c9
ingisaacgutierrez <igutierrezsaltos@gmail.com>
2023-09-19 22:55:33 -0500

    Complete prep5 Learning Activity

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,43 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        DisplayMessage();
+
+        string name = DisName();
+        int number = DisNum();
+
+        int sqresult = SqNumber(number);
+
+        DisplayResult(name, sqresult);
+
+    }
+    static void DisplayMessage()
+    {
+        Console.WriteLine("Welcome to the program!");
+    }
+    static string DisName()
+    {            
+        Console.Write("Please enter your name: ");
+        string username = Console.ReadLine(); 
+
+        return username;
+    }
+    static int DisNum()
+    {
+        Console.Write("Please enter your favorite number: ");
+        int num = int.Parse(Console.ReadLine());
+
+        return num;
+
+    }
+    static int SqNumber(int num)
+    {
+        int square = num * num;
+        return square;
+
+    }
+    static void DisplayResult(string name, int square)
+    {
+        Console.WriteLine($"{name}, the square of your number is {square}");
     }
 }
