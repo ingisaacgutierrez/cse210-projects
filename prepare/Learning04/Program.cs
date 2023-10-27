9afc5f320a00c8aae2da0850548084069aa8375d
ingisaacgutierrez <igutierrezsaltos@gmail.com>
2023-10-26 20:13:50 -0500

    writing Program main class

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Assignment assignment = new Assignment("Samuel Bennet" , "Multiplicaction");
+        Console.WriteLine(assignment.GetSummary());
+       
+        Console.WriteLine();
+
+        MathAssignment ma1 = new MathAssignment("7.3","8-19", "Roberto Rodriguez","Fractions");
+        Console.WriteLine(ma1.GetSummary());
+        Console.WriteLine(ma1.GetHomeworkList());
+
+        Console.WriteLine();
+
+        WritingAssignment wa = new WritingAssignment("The causes of Wolrd War II by Mary Waters", "Mary Waters", "European History");
+        Console.WriteLine(wa.GetSummary());
+        Console.WriteLine(wa.GetWritingInformation());
+
+        Console.WriteLine();
+
+        
     }
 }
