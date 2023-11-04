830ce2491b8f677cb7800594759f2484610139c2
ingisaacgutierrez <igutierrezsaltos@gmail.com>
2023-11-04 10:23:58 -0500

    add Reflection activity optino

--- prove/Develop04/Program.cs
+++ prove/Develop04/Program.cs
@@ -13,13 +13,13 @@ class Program
                     "\n  3. Start Listing Activity" +
                     "\n  4. Quit"
                     );
-                Console.WriteLine("Select a choice from the menu:");
+                Console.WriteLine("\nSelect a choice from the menu:");
                 menuOption = Console.ReadLine();
                 Console.Clear();  
                 switch (menuOption)
                 {
                     case "1":
-                    BreathingActivity ba = new BreathingActivity("Breathing Activity", "This activity will help you reflect on times in your life when you have shown strength and resilience. This will help you recognize the power you have and how you can use it in other aspects of your life.",0);
+                    BreathingActivity ba = new BreathingActivity("Breathing Activity", "\nThis activity will help you reflect on times in your life when you have shown strength and resilience. This will help you recognize the power you have and how you can use it in other aspects of your life.",0);
                     ba.beginMessage();
                     int duration = Convert.ToInt32(Console.ReadLine());
                     BreathingActivity baTime = new BreathingActivity("Breathing Activity", "",duration);
@@ -27,22 +27,32 @@ class Program
                     Console.WriteLine("Get ready...");
                     baTime.showingSpinner();
                     baTime.showBreathingActivity();
-                    //baTime.pauseCountdownTimer();
-                    Console.WriteLine("Well done...");
+                    Console.WriteLine("\nWell done...");
                     baTime.showingSpinner();
                     baTime.endMessage();
                     baTime.showingSpinner();
                         break;
                     case "2":
-                   
-                    Console.WriteLine("option2");
+                    ReflectionActivity ra = new ReflectionActivity("Reflection Activity", "\nThis activity will help you reflect on times in your life when you have shown strength and resilience. This will help you recognize the power you have and how you can use it in other aspects of your life.",0);
+                    ra.beginMessage();
+                    duration = Convert.ToInt32(Console.ReadLine());
+                    ReflectionActivity raTime = new ReflectionActivity("Reflection Activity", "",duration);
+                    Console.Clear();
+                    Console.WriteLine("Get ready...");
+                    raTime.showingSpinner();
+                    raTime.DisplayPrompt();
+                    raTime.DisplayQuestion();
+                    Console.WriteLine("\nWell done...");
+                    raTime.showingSpinner();
+                    raTime.endMessage();
+                    raTime.showingSpinner();
                         break;
                     case "3":
                     
                     Console.WriteLine("option3");
                         break;
                     case "4":
-                    
+                    Environment.Exit(0);
                         break;
                     default:
                         break;
