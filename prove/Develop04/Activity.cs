c6034719cb91113cfac022b6059f866c5d751baf
ingisaacgutierrez <igutierrezsaltos@gmail.com>
2023-11-04 09:36:40 -0500

    Activity class complete

--- prove/Develop04/Activity.cs
+++ prove/Develop04/Activity.cs
@@ -2,25 +2,79 @@ using System;
 
 class Activity
 {
-    private string name;
-    private string description;
-    private int duration;
+    private string _name;
+    private string _description;
+    private int _duration;
 
+    public Activity(string name, string description, int duration)
+    {
+        _name = name;
+        _description = description;
+        _duration = duration;
+    }
+
+    public string GetName()
+    {
+        return _name;
+    }
+    public string GetDescription()
+    {
+        return _description;
+    }
+    public int GetDuration()
+    {
+        return _duration;
+    }
     public void beginMessage()
     {
+       Console.WriteLine($"Welcome to the {_name}"+$"\n {_description}");
+       Console.WriteLine("How long, in seconds, would you like for your session?");
 
     }
     public void endMessage()
     {
-
+        Console.WriteLine($"You have completed another {_duration} seconds of the {_name}");
     }
     public void showingSpinner()
     {
+       List<string> spinner = new List<string>();
+       spinner.Add("|");
+       spinner.Add("/");
+       spinner.Add("-");
+       spinner.Add("\\");
+       spinner.Add("|");
+       spinner.Add("/");
+       spinner.Add("-");
+       spinner.Add("\\");
 
+       DateTime startTime = DateTime.Now;
+       DateTime endTime = startTime.AddSeconds(8);
+
+       int i = 0;
+
+       while (DateTime.Now < endTime)
+       {
+        string s = spinner[i];
+        Console.Write(s);
+        Thread.Sleep(500);
+        Console.Write("\b \b");
+        i++;
+
+        if (i>= spinner.Count)
+        {
+            i = 0 ;
+        }
+       }
     }
-    public void pauseCountdownTimer()
+    public void pauseCountdownTimer(int duration)
     {
-        
-    }
+    
+        for (int i=duration; i>0;i--)
+        {
+            Console.Write(i % 8);
+            Thread.Sleep(1000);
+            Console.Write("\b \b");
 
+        }
+    }
 }
