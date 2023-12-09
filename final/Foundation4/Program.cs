a7dabe49e8a55f527cc4da17f8d3904e3abe7047
ingisaacgutierrez <igutierrezsaltos@gmail.com>
2023-12-08 19:32:59 -0500

    main class program 4

--- final/Foundation4/Program.cs
+++ final/Foundation4/Program.cs
@@ -4,6 +4,18 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Foundation4 World!");
+        List<Activity> act = new List<Activity>();
+        Running run = new Running(DateTime.Now, "Running",30, 4.0);
+        Cycling cy = new Cycling(DateTime.Now, "Cycling", 60,35);
+        Swimming sw = new Swimming(DateTime.Now,"Swimming",30,20);
+        act.Add(run);
+        act.Add(cy);
+        act.Add(sw);
+        Console.WriteLine();
+        foreach (Activity activity in act)
+        {
+            Console.WriteLine(activity.getSummary());
+        }
+        Console.WriteLine();
     }
 }
